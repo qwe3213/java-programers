03fa837471b6d6d91ae9e21547fa19fa2d18216f
hayun <hs183000@naver.com>
2022-12-27 21:02:07 +0900

    자연수뒤집어배열로만들기 문제풀이

---
+++ java-programers/src/자연수뒤집어배열로만들기.java
@@ -0,0 +1,22 @@
+class 자연수뒤집어배열로만들기 {
+    public int[] solution(long n) {
+
+        long num = n;
+        int cnt = 0;
+
+        while(num != 0){
+            num /= 10;
+            cnt++;
+        }
+
+        int[] answer = new int[cnt];
+
+        num = n;
+        for(int i = 0; num != 0; i++){
+            answer[i] = (int)(num % 10);
+            num /= 10;
+        }
+
+        return answer;
+    }
+}
