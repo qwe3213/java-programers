4c2f9e1ecb342303a4f0af2620144e276f126a3f
hayun <hs183000@naver.com>
2023-02-01 23:03:31 +0900

    x만큼간격이있는n개의숫자 문제풀이

---
+++ java-programers/src/X만큼간격이있는N개의숫자.java
@@ -0,0 +1,10 @@
+public class X만큼간격이있는N개의숫자 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = x;
+        for(int i = 0 ; i <n; i++){
+            answer[i] = num * (i+1);
+        }
+        return answer;
+    }
+}
