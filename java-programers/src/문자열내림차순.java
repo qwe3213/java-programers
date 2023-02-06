51c29630d0913e13c007af2dcabb01f1a279c9c8
hayun <hs183000@naver.com>
2023-02-06 21:07:03 +0900

    문자열내림차순 문제풀이

--- java-programers/src/문자열내림차순.java
+++ java-programers/src/문자열내림차순.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class 문자열내림차순 {
+import java.util.Arrays;
+import java.util.Collections;
+public class 문자열내림차순 {
+    class Solution {
+        public String solution(String s) {
+            String answer = "";
+
+            String[] str = s.split("");
+            Arrays.sort(str, Collections.reverseOrder());
+
+            for(String a : str)
+                answer += a;
+
+            return answer;
+        }
 }
