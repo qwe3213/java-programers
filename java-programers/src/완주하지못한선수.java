d55b91a734cc74ac659a67a7f656cff2da086ff7
hayun <hs183000@naver.com>
2023-02-25 22:06:04 +0900

    완주하지못한선수 문제풀이

---
+++ java-programers/src/완주하지못한선수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class 완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String,Integer> map = new HashMap<>();
+
+        for(String player : participant) map.put(player, map.getOrDefault(player,0)+1);
+        for(String player : completion) map.put(player, map.get(player)-1);
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue() !=0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
