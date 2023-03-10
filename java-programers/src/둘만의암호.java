6dd748a43883cd9dc124b0c3c2b52b34cc89a7bb
hayun <hs183000@naver.com>
2023-03-10 22:41:07 +0900

    둘만의암호 문제풀이

---
+++ java-programers/src/둘만의암호.java
@@ -0,0 +1,15 @@
+public class 둘만의암호 {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        for(int i=0;i<s.length();i++) {
+            char temp = s.charAt(i);
+            for(int j=0;j<index;j++) {
+                temp+=1;
+                if(temp>122) temp -=26;
+                if(skip.contains(String.valueOf(temp))) j--;
+            }
+            answer+=temp;
+        }
+        return answer;
+    }
+}
