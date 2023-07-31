74ad24dea4226fa68b43182533480c44d9a62483
hayun <hs183000@naver.com>
2023-07-31 21:21:28 +0900

    n진수게임 문제풀이

---
+++ java-programers/src/n진수게임.java
@@ -0,0 +1,17 @@
+public class n진수게임 {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder convert = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+
+        for(int i = 0; convert.length() <= t * m; i++){
+            convert.append(Integer.toString(i, n));
+        }
+
+        for(int i = p - 1;  answer.length() < t; i += m){
+            answer.append(convert.charAt(i));
+        }
+
+        return answer.toString().toUpperCase();
+    }
+
+}
